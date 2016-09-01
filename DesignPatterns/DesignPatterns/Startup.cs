606b63daf7e0174e71f7b2a14d4a50f92b5bbec5
quoc-cuong <quoc-cuong@it-developers.com>
2016-09-01 16:23:50 +0700

    Init

---
+++ DesignPatterns/DesignPatterns/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DesignPatterns.Startup))]
+namespace DesignPatterns
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
